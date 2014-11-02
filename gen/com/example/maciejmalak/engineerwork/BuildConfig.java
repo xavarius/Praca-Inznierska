80868c1b64cfa0805750f9e5f6e954c68602e30a
Maciej Malak <maciejmalak@gmail.com>
2014-11-02 17:09:05 +0100

    Dziala wyswietlenie mapy, zlokalizowanie telefonu za pomoca funkcji googla oraz menu i inne elementy UI

---
+++ gen/com/example/maciejmalak/engineerwork/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.maciejmalak.engineerwork;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
